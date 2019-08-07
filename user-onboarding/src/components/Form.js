7a2fdf42bf6e45a86bfba489657dc0e8db9b9275
Vicky <vyue013@gmail.com>
2019-08-07 14:38:00 -0700

    implemented form validation and error messaging

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -3,17 +3,29 @@ import axios from 'axios';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 
-const UserForm = () => {
+const UserForm = ({ errors, touched, values }) => {
     return (
         <div className="user-onboard">
-            <h1>Welcome</h1>
+            <h1>Welcome!</h1>
             <Form>
                 <Field type="text" name="name" placeholder="Name" />
+                {touched.name && errors.name && (
+                    <p className="error">{errors.name}</p>
+                )}
+
                 <Field type="text" name="email" placeholder="Email" />
+                {touched.email && errors.email && (
+                    <p className="error">{errors.email}</p>
+                )}
+
                 <Field type="password" name="password" placeholder="Password" />
+                {touched.password && errors.password && (
+                    <p className="error">{errors.password}</p>
+                )}
+
                 <label className="checkbox-container">
                     Terms of Service
-                    <Field type="checkbox" name="terms of service" />
+                    <Field type="checkbox" name="tos" checked={values.tos} />
                     <span  className="checkmark" />
                 </label>
                 <button type="submit">Submit</button>
@@ -23,6 +35,29 @@ const UserForm = () => {
 };
 
 const FormikUserForm = withFormik({
+    mapPropsToValues({ name, email, password, tos }) {
+        return {
+            tos: tos || false,
+            name: name || '',
+            email: email || '',
+            password: password || ''
+        }
+    },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string()
+            .required('Name is required'),
+        email: Yup.string()
+            .email('Email not valid')
+            .required('Email is required'),
+        password: Yup.string()
+            .min(8, "Password must be 8 characters or longer")
+            .required('Password is required')
+    }),
+
+    handleSubmit(values) {
+        console.log('form submitted', values);
+    }
 
 })(UserForm);
 
