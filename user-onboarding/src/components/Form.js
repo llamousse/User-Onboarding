86ea38c67100821f1090d0cf054d2cda9af7aa11
Vicky <vyue013@gmail.com>
2019-08-07 13:23:52 -0700

    project set up, created Form component, imported to App

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+import { Form } from 'formik';
+import * as Yup from 'yup';
+
+const UserForm = () => {
+    return (
+        <div>User Form</div>
+    );
+};
+
+export default UserForm;
